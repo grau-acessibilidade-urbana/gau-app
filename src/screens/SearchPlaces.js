e34c2271f070090e81e011cac1e6d80bca904a3c
rogerdmorales@hotmail.com <rogerdmorales@hotmail.com>
2020-04-30 23:50:49 -0300

    Incluindo botão de localização atual do usuário

--- src/screens/SearchPlaces.js
+++ src/screens/SearchPlaces.js
@@ -5,19 +5,40 @@ import {
     TouchableOpacity,
     Dimensions,
     StyleSheet,
-    TextInput
+    TextInput,
+    Alert
 } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Callout } from 'react-native-maps'
-import Icon from 'react-native-vector-icons/FontAwesome'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import Geocoder from 'react-native-geocoding'
+import Geolocation from '@react-native-community/geolocation';
 
 const { height, width } = Dimensions.get('window');
 
 export default class SearchPlaces extends Component {
 
     state = {
-        currentLocation: { latitude: -23.55, longitude: -46.83 }
+        region: {
+            latitude: -23.55,
+            longitude: -46.83,
+            latitudeDelta: 0.001,
+            longitudeDelta: 0.001
+        }
     };
+
+    getLocation = () => {
+        Geolocation.getCurrentPosition(position => {
+            this.setState({
+                region: {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    latitudeDelta: 0.001,
+                    longitudeDelta: 0.001
+                }
+            });
+        }, err => Alert.alert('Erro', err.toString()));
+    }
 
     render() {
         return (
@@ -25,27 +46,26 @@ export default class SearchPlaces extends Component {
                 <MapView
                     provider={PROVIDER_GOOGLE}
                     style={styles.map}
+                    showsUserLocation
                     loadingEnabled={true}
-                    region={{
-                        latitude: this.state.currentLocation.latitude,
-                        longitude: this.state.currentLocation.longitude,
-                        latitudeDelta: 0.015,
-                        longitudeDelta: 0.0121,
-                    }}
-                />
-                <Callout>
-                    <View style={styles.searchMenu}>
-                        <TouchableOpacity style={styles.menuButton} onPress={() => this.props.navigation.openDrawer()}>
-                            <Icon name='bars' size={25} color='#FFF'></Icon>
-                        </TouchableOpacity>
-                        <View style={styles.search}>
-                            <TextInput placeholder='Digite sua pesquisa...' style={styles.input} />
-                            <TouchableWithoutFeedback>
-                                <Icon name='search' size={25} color='#3197b5'></Icon>
-                            </TouchableWithoutFeedback>
-                        </View>
+                    region={this.state.region}
+                    onRegionChange={this.onRegionChange}>
+                </MapView>
+                <Callout style={styles.searchMenu}>
+                    <TouchableOpacity style={styles.menuButton} onPress={() => this.props.navigation.openDrawer()}>
+                        <Icon name='menu' size={25} color='#FFF'></Icon>
+                    </TouchableOpacity>
+                    <View style={styles.search}>
+                        <TextInput placeholder='Digite sua pesquisa...' style={styles.input} />
+                        <TouchableWithoutFeedback>
+                            <Icon name='search' size={25} color='#3197b5'></Icon>
+                        </TouchableWithoutFeedback>
                     </View>
-
+                </Callout>
+                <Callout style={styles.gpsContainer}>
+                    <TouchableOpacity style={styles.gpsButton} onPress={this.getLocation}>
+                        <Icon name='gps-fixed' size={30} color='#FFF'></Icon>
+                    </TouchableOpacity>
                 </Callout>
             </View>
         )
@@ -79,7 +99,7 @@ const styles = StyleSheet.create({
     search: {
         flexDirection: 'row',
         backgroundColor: '#FFF',
-        alignItems:'center',
+        alignItems: 'center',
         width: width * 0.75,
         height: 50,
         borderRadius: 15,
@@ -95,5 +115,20 @@ const styles = StyleSheet.create({
         color: '#3197b5',
         fontSize: 18,
         padding: 5,
+    },
+    gpsContainer: {
+        position: 'absolute',
+        bottom: 50,
+        right: 15,
+        width: 50,
+        height: 50,
+    },
+    gpsButton: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#3197b5',
+        borderRadius: 25,
     }
 })
