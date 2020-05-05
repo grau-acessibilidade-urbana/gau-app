18d668d7a761fd49df7ab365f4117e271009bf9f
Roger Doria <rmorales.vermont@alelo.com.br>
2020-05-04 21:40:49 -0300

    React native autocomplete

--- android/app/src/main/java/com/gau/MainApplication.java
+++ android/app/src/main/java/com/gau/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
