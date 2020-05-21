c85cd4ce9670f35a8925309a0d0cf47f9684be09
Roger Doria <rmorales.vermont@alelo.com.br>
2020-05-21 12:29:36 -0300

    Incluindo arquivo de propriedades

---
+++ config.js
@@ -0,0 +1,3 @@
+export default config = {
+    GOOGLE_API_KEY: 'AIzaSyA-1Tg-e61BNPrMgy-FXpVH3wAZnIhKV48'
+}
--- src/api/google.js
+++ src/api/google.js
@@ -1,10 +1,10 @@
 import axios from "axios";
+import Config from '../../config';
 
-const API_KEY = 'AIzaSyA-1Tg-e61BNPrMgy-FXpVH3wAZnIhKV48';
 axios.defaults.baseURL = 'https://maps.googleapis.com/maps/api'
 
 export function getPlacesPredictions(query, currentLocation) {
-    return axios.get(`/place/autocomplete/json?input=${query}&types=establishment&components=country:br&key=${API_KEY}`)
+    return axios.get(`/place/autocomplete/json?input=${query}&types=establishment&components=country:br&key=${Config.GOOGLE_API_KEY}`)
         .then(res => {
             if (res && res.data && res.data.predictions) {
                 return res.data.predictions.map(prediction => {
@@ -19,13 +19,13 @@ export function getPlacesPredictions(query, currentLocation) {
 }
 
 export function getImageByPhotoReference(photoReference) {
-    return axios.get(`/place/photo?maxwidth=400&photoreference=${photoReference}&key=${API_KEY}`)
+    return axios.get(`/place/photo?maxwidth=400&photoreference=${photoReference}&key=${Config.GOOGLE_API_KEY}`)
         .then(res => res.request.responseURL)
         .catch(err => console.log(err));
 }
 
 export function getPlaceDetailsById(currentLocation, id) {
-    return axios.get(`/place/details/json?place_id=${id}&key=${API_KEY}`)
+    return axios.get(`/place/details/json?place_id=${id}&key=${Config.GOOGLE_API_KEY}`)
         .then(async (res) => {
             if (res && res.data && res.data.result) {
                 const result = res.data.result;
@@ -52,7 +52,7 @@ export function getPlaceDetailsById(currentLocation, id) {
 }
 
 export function findNearbyPlacesByText(location, text) {
-    return axios.get(`/place/nearbysearch/json?location=${location.latitude},${location.longitude}&radius=50000&type=establishment&keyword=${text}&key=${API_KEY}`)
+    return axios.get(`/place/nearbysearch/json?location=${location.latitude},${location.longitude}&radius=50000&type=establishment&keyword=${text}&key=${Config.GOOGLE_API_KEY}`)
         .then(res => {
             if (res && res.data && res.data.results) {
                 const results = res.data.results;
@@ -72,7 +72,7 @@ export function findNearbyPlacesByText(location, text) {
 }
 
 export function getDistanceBetweenPlaces(origin, destination) {
-    return axios.get(`/distancematrix/json?origins=${origin.latitude},${origin.longitude}&destinations=${destination.latitude},${destination.longitude}&key=${API_KEY}`)
+    return axios.get(`/distancematrix/json?origins=${origin.latitude},${origin.longitude}&destinations=${destination.latitude},${destination.longitude}&key=${Config.GOOGLE_API_KEY}`)
         .then(res => {
             if (res && res.data && res.data.rows && res.data.rows.length > 0) {
                 const distance = res.data.rows
