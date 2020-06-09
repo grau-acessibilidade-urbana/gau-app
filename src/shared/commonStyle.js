f4713a39f3bbef8ed9bc6d1ae63b4651b680db1c
evehste <evehste>
2020-06-08 22:45:31 -0300

    Atualizando cores

--- src/shared/commonStyle.js
+++ src/shared/commonStyle.js
@@ -7,6 +7,7 @@ export default {
         secondFontColor: '#8B8B8B',
         FullStar: '#FFC107',
         EmptyStar: '#D1D1D1',
+        backgroundStar: '#FFEFC0',
         whiteTransparent: 'rgba(255, 255, 255, 0.65)',
 
     } 
@@ -19,4 +20,5 @@ export default {
 // primaryFontColor = Cor principal das fontes do app (Cinza)
 // secondFontColor = Textos com menos destaque no app ou icones de tom claro (cinza claro)
 // FullStar = cor das estrelas preenchidas (amarelo)
+// backgroundStar = para box que tem estrelas (amarelo bem claro)
 // EmptyStar = cor das estrelas vazias (cinza claro)
