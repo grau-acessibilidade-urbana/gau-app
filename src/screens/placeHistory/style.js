10cf6723976ab1636f2dc856af6c36c1dbf3ab55
evehste <evehste>
2020-06-08 22:43:44 -0300

    Atualizando cores

---
+++ src/screens/placeHistory/style.js
@@ -0,0 +1,67 @@
+import { StyleSheet, Dimensions } from 'react-native';
+import commonStyle from "../../shared/commonStyle";
+const { height, width } = Dimensions.get('window');
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    image: {
+        height: '35%',
+        backgroundColor: 'aqua',
+        resizeMode: "cover",
+        zIndex: -10
+    },
+    details: {
+        height: '70%',
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30, 
+        marginTop: '-10%'
+    },
+    containerTitle: {
+        flexDirection: "row",
+        padding: 10,
+        marginTop: 7
+    },
+    title: {
+        flex: 1,
+        textAlign: "center",
+        fontSize: 20,
+        color: commonStyle.colors.primaryColor
+    },
+    containerStar: {
+        flexDirection: "row",
+        backgroundColor: commonStyle.colors.backgroundStar,
+        borderRadius: 5,
+        justifyContent: "center",
+        alignItems: "center",
+        marginRight: 5
+    },
+    rating: {
+        color: commonStyle.colors.primaryFontColor,
+        paddingRight: 3,
+        fontSize: 16
+    }, comment:  {
+        textAlign: "justify",
+        color: commonStyle.colors.primaryFontColor,
+        paddingHorizontal: 15,
+        fontSize: 15
+    },
+    btnEdit: {
+        backgroundColor: commonStyle.colors.primaryColor,
+        flex: 1,
+        margin: 10,
+        marginBottom: 20,
+        padding: 10,
+        borderRadius: 5
+    },
+    textEdit: {
+        color: "#fff",
+        flex: 1,
+        textAlign: "center",
+        fontSize: 16
+    }
+    
+})
+
+export default styles
